b058107474daa643b855a219a1d2df5a0266ca2d
Marco Rossel <andresmarcorossel@gmail.com>
2022-08-06 01:20:59 -0400

    finished 1

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ const futureTime = futureDate.getTime();
 function getRemainingTime(){
   const today = new Date().getTime();
   const t = futureTime - today;
-  console.log(t);
   // 1s = 1000ms
   // 1m = 60s
   // 1hr = 60min
@@ -74,7 +73,9 @@ function getRemainingTime(){
   items.forEach(function(item, index) {
     item.innerHTML = values[index];
   });
-  
+
 }
+// contdown
+let countdown = setInterval(getRemainingTime, 1000)
 
 getRemainingTime(); 
